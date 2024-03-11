5762bb6fe95fbb6790e505f6cec2d3b4eab92a89
dru0pa <dru0pa@gmail.com>
2024-03-11 13:38:38 +0200

    Update Install-Pre.ps1

--- Install-Pre.ps1
+++ Install-Pre.ps1
@@ -1,3 +1,14 @@
+#A script to Install some of the prerequisites for CyberArk.
+#The script looks to install Chrome Enterprise.
+#The script looks to install NotePad++(update the version as required)
+#The script looks to install both vcredist_x64.exe and vcredist_x86.exe
+#The script will install ndp48-x86-x64-allos-enu.exe as well
+#It will also disable updates to Chrome via Service and ScheduledTask by disabling them.
+#Writen By Andrew Price.
+
+
+
+
 # Define script location
 $scriptPathG = Get-Location
 
